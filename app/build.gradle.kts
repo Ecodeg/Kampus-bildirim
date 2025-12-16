7263b3e4e0fe25043e4245b42ad16a0657feea27
Ebrar <ebrargum@gmail.com>
2025-12-16 15:47:16 +0300

    Konum izinleri eklendi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
@@ -65,4 +66,9 @@ dependencies {
 
     // Messaging (bildirimler)
     implementation("com.google.firebase:firebase-messaging")
+
+    // Konum izinleri
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+    implementation("com.google.android.gms:play-services-location:21.0.1")
 }
